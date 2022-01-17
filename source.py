2ae943b70dd5893665b5eced1d78031431f3739f
reazura <mackyztolentino@gmail.com>
2022-01-17 18:24:36 +0800

    script

--- source.py
+++ source.py
@@ -4,68 +4,56 @@ import cv2
 import numpy
 from time import time, sleep
 import pyautogui
+import random
 
+max=38
 pyautogui.PAUSE = 0
 
-print("Press 's' to start playing.")
 print("Once started press 'q' to quit.")
-keyboard.wait('s')
-left = True
-x = 340
-y = 850
 sct = mss.mss()
-dimensions_left = {
-        'left': 290,
-        'top': 600,
-        'width': 150,
-        'height': 250
+dimensions = {
+        'left': 1850,
+        'top': 977,
+        'width': 31,
+        'height': 31
     }
 
-dimensions_right = {
-        'left': 520,
-        'top': 600,
-        'width': 150,
-        'height': 250
-    }
+img_catch = cv2.imread('catch.png')
+img_cast = cv2.imread('cast.png')
 
-wood_left = cv2.imread('woodleft.jpg')
+fishing=False
+action=""
+count=0
 
-wood_right = cv2.imread('woodright.jpg')
-w = wood_left.shape[1]
-h = wood_left.shape[0]
-
-fps_time = time()
 while True:
-
-    if left:
-        scr = numpy.array(sct.grab(dimensions_left))
-        wood = wood_left
+    if fishing:
+        img_target = cv2.imread('catch.png')
+        action = "Catching"
     else:
-        scr = numpy.array(sct.grab(dimensions_right))
-        wood = wood_right
+        img_target = cv2.imread('cast.png')
+        action = "Casting"
+
+    scr = numpy.array(sct.grab(dimensions))
 
     # Cut off alpha
     scr_remove = scr[:,:,:3]
+    result = cv2.matchTemplate(scr_remove, img_target, cv2.TM_CCOEFF_NORMED)
 
-    result = cv2.matchTemplate(scr_remove, wood, cv2.TM_CCOEFF_NORMED)
-    
     _, max_val, _, max_loc = cv2.minMaxLoc(result)
-    print(f"Max Val: {max_val} Max Loc: {max_loc}")
+    print(f"{action}. ({count+1} of {max}) Match%: {max_val}")
     src = scr.copy()
     if max_val > .85:
-        left = not left
-        if left:
-            x=340
-        else:
-            x=600
-        cv2.rectangle(scr, max_loc, (max_loc[0] + w, max_loc[1] + h), (0,255,255), 2)
+        fishing = not fishing
+        randX = random.randint(0,21)
+        randY = random.randint(0,21)
+        pyautogui.click(x=1855+randX, y=982+randY)
+        if not fishing:
+            count+=1
+            if count==max:
+                print(f"End")
+                break
 
-    cv2.imshow('Screen Shot', scr)
-    cv2.waitKey(1)
-    pyautogui.click(x=x, y=y)
-    sleep(.10)
+    sleep_time = round(random.uniform(.05,.15), 4)
+    sleep(sleep_time)
     if keyboard.is_pressed('q'):
         break
-
-    print('FPS: {}'.format(1 / (time() - fps_time)))
-    fps_time = time()
